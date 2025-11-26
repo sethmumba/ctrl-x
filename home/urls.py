69a65294b4c84b1866d114adbb2de03c8a25f42b
unknown <Seth Mumba>
2025-11-26 16:15:06 +0300

    added tracing

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    # urls.py
+path('order/daily-order-status-json/', views.daily_order_status_json, name='daily_order_status_json')
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.utils import timezone
+from orders.models import Order
+from django.http import JsonResponse
+from django.utils import timezone
 
 def home_page(request):
-    return render(request, 'home/home.html')
+    today = timezone.now().date()
+    daily_orders_count = Order.objects.filter(created_at__date=today).count()
+    daily_limit = 6
+
+    return render(request, 'home/home.html', {
+        'daily_orders_count': daily_orders_count,
+        'daily_limit': daily_limit,
+    })
+
+
+# views.py
+def daily_order_status_json(request):
+    today = timezone.now().date()
+    daily_orders_count = Order.objects.filter(created_at__date=today).count()
+    return JsonResponse({'daily_orders_count': daily_orders_count})
