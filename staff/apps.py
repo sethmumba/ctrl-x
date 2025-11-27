15d4347941c2f4f18a79140ea1f6778fa514af9b
unknown <Seth Mumba>
2025-11-27 19:11:55 +0300

    fixed store assignment

--- staff/apps.py
+++ staff/apps.py
@@ -1,9 +1,8 @@
 from django.apps import AppConfig
 
-
 class StaffConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'staff'
 
-def ready(self):
-    import staff.signals
+    def ready(self):
+        import staff.signals  # <- inside the class!
