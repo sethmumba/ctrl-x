2908f62e554c92463444a20175339704dd6c405d
unknown <Seth Mumba>
2025-11-29 21:24:53 +0300

    railway postgress

--- droppify/urls.py
+++ droppify/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('order/', include('orders.urls')),
@@ -13,3 +13,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),   # home + dashboard pages
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns  += staticfiles_urlpatterns()
